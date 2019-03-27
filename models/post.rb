4391e3e24d9cd0ae1fbfa5dcf166d17c04a28d88
Thomas Müller <th.mueller87@gmail.com>
2019-03-27 13:31:56 +0100

    Talk to Alfred

--- models/post.rb
+++ models/post.rb
@@ -1,6 +1,6 @@
 require 'sequel'
 
-POSTGRES_URL = 'postgres://postgres@127.16.12.124:5432/rubychat'.freeze
+POSTGRES_URL = 'postgres://postgres@192.168.250.253:5432/rubychat'.freeze
 DB ||= Sequel.connect(POSTGRES_URL, max_connections: 10)
 
 class Post < Sequel::Model(:posts)
